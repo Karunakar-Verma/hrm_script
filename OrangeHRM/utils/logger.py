6c7199c9a2bc411f0ff0136102b05e22f76f8377
Automation Engineer <automationengineer@gmail.com>
2026-06-11 10:43:36 +0530

    Updated logger file

--- OrangeHRM/utils/logger.py
+++ OrangeHRM/utils/logger.py
@@ -1,15 +1,20 @@
 import logging
-from logging import FileHandler
+import os
 
-
-location = r"OrangeHRM\Logs\Orange.log"
 def get_logger():
-    logger = logging.getLogger("OrangeHRM")
+
+    log_dir = "Logs"
+    os.makedirs(log_dir, exist_ok=True)
+
+    log_file = os.path.join(log_dir, "Orange.log")
+
+    logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
     if not logger.handlers:
-        file_handler = logging.FileHandler(location)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler = logging.FileHandler(log_file)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
+
     return logger
